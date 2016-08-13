6235ed2ac1941cb1cbe523a74fc0747a3b00f216
Lazzaro83 <casadifianco@gmail.com>
2016-08-13 16:15:38 +0200

    Not the final version.

--- javascripts/main.js
+++ javascripts/main.js
@@ -16,7 +16,7 @@
 			div = $divs[i];
 			offset = top - offsets[i].top;
 			scroll = ~~(offset/liHeight * diffHeight);
-			transform = 'translate3d(0px' + scroll + 'px, 0px)';
+			transform = 'translate3d(0px, ' + scroll + 'px, 0px)';
 			div.style.webkitTransform = transform;
 			div.style.MozTransform = transform;
 			div.style.msTransform = transform;
@@ -29,6 +29,32 @@
 		requestAnimationFrame(loop);
 		render();
 	})();
+
+	(function() {
+    var lastTime = 0;
+    var vendors = ['webkit', 'moz'];
+    for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+        window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+        window.cancelAnimationFrame =
+          window[vendors[x]+'CancelAnimationFrame'] || window[vendors[x]+'CancelRequestAnimationFrame'];
+    }
+
+    if (!window.requestAnimationFrame)
+        window.requestAnimationFrame = function(callback, element) {
+            var currTime = new Date().getTime();
+            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+            var id = window.setTimeout(function() { callback(currTime + timeToCall); },
+              timeToCall);
+            lastTime = currTime + timeToCall;
+            return id;
+        };
+
+    if (!window.cancelAnimationFrame)
+        window.cancelAnimationFrame = function(id) {
+            clearTimeout(id);
+        };
+}());
+
 
 
 
